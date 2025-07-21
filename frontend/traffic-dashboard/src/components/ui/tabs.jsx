de2a4136fd563c6e6b0707efa763bef3f0d0d669
Themal Lakruwan <themaldesilva2001@gmail.com>
2025-07-22 01:16:51 +0530

    fixed tabs issue in frontend

--- frontend/traffic-dashboard/src/components/ui/tabs.jsx
+++ frontend/traffic-dashboard/src/components/ui/tabs.jsx
@@ -1,4 +1,6 @@
-import { cn } from "@/lib/utils"
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import React from "react";
+import { cn } from "@/lib/utils";
 
 const Tabs = TabsPrimitive.Root
 
